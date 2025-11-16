f96a524fc611c1934bbff61e88424268209906f3
tashidzen <tashidzen@yandex.ru>
2025-11-16 22:26:16 +0300

    Приложение подключено к API.

--- writeFunction.js
+++ writeFunction.js
@@ -1,4 +1,4 @@
-import { comments } from './comments.js';
+import { updateComments } from './comments.js';
 import { renderComments } from './renderComments.js';
 import { formatDate } from './formatDate.js';
 import { replace } from './replaceAll.js';
@@ -18,19 +18,32 @@ export function writeFunction() {
         }
 
         const verifiedName = replace(name);
-
         const verifiedComment = replace(comment);
 
         const newComment = {
-            userName: verifiedName,
+            name: verifiedName,
             date: formatDate(currentDate),
-            commentText: verifiedComment,
-            count_likes: 0,
-            status: false,
+            text: verifiedComment,
+            likes: 0,
+            isLiked: false,
         };
 
-        comments.push(newComment);
-        renderComments(comments);
+        fetch('https://wedev-api.sky.pro/api/v1/natalya-gromova/comments', {
+            method: 'POST',
+            body: JSON.stringify(newComment),
+        })
+            .then(() => {
+                return fetch(
+                    'https://wedev-api.sky.pro/api/v1/natalya-gromova/comments',
+                );
+            })
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                updateComments(data.comments);
+                renderComments(data.comments);
+            });
 
         enteredName.value = '';
         enteredComment.value = '';
